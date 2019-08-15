d24fa98c0af21b2d0e42421d9826b62d514b0a13
Dipl. Ing. Péter Varkoly <peter@varkoly.de>
2019-08-15 16:42:54 +0200

    Do not create certificat with longer validity then the CA itself. New script to set the dyndns.

---
+++ plugins/validate_regcode/100-set-ip.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import json
+import os
+import sys
+from configobj import ConfigObj
+config   = ConfigObj("/opt/oss-java/conf/oss-api.properties")
+passwd   = config['de.openschoolserver.dao.User.Register.Password']
+dyndns=""
+
+#Create an open task if something goes wrong
+def create_open_task():
+  TASK = "/var/adm/oss/opentasks/100-validate-regcode-" + os.popen('uuidgen -t').read().rstrip()
+  with open(TASK, "w") as f:
+    f.write(json.dumps(dyndns,sort_keys=True,ensure_ascii=False,encoding="utf-8"))
+
+dyndns=json.loads(sys.stdin.read())
+
+ip=dyndns['ip']
+hostname=dyndns['hostname']
+domain=dyndns['domain']
+oldip=dyndns.get('oldip','')
+
+if oldip == '':
+    if os.system("samba-tool dns add localhost " + domain + " " + hostname + " A " + ip + "  -U register%" + passwd ) != 0:
+       create_open_task()
+elif oldip != ip:
+    if os.system("samba-tool dns update localhost " + domain + " " + hostname + " A " + oldip + " " + ip + "  -U register%" + passwd ) != 0:
+       if os.system("samba-tool dns add localhost " + domain + " " + hostname + " A " + ip + "  -U register%" + passwd ) != 0:
+          create_open_task()
+
