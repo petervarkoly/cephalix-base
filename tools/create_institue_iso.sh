28a6a3999e77b049d43f0c75e196137e479f6aa7
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2020-08-19 07:16:15 +0200

    Use susecd instead of mkisofs

--- tools/create_institue_iso.sh
+++ tools/create_institue_iso.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 uuid=$1
-tmpDir=$( mktemp /tmp/mkisoXXXXX )
 cp /srv/www/admin/configs/${uuid}.xml /usr/share/cephalix/templates/iso/cranix.xml
-mkisofs --publisher 'Dipl.Ing. Peter Varkoly' -p 'CD-Team, http://www.cephalix.eu' \
-        -r -J -f -pad -no-emul-boot -boot-load-size 4 -boot-info-table -b boot/x86_64/loader/isolinux.bin  \
-        -c boot/boot.catalog -hide boot/boot.catalog -hide-joliet boot/boot.catalog -A - -V SU.001 \
-	-o /srv/www/admin/isos/${uuid}.iso $tmpDir /usr/share/cephalix/templates/iso/ >/dev/null 2>&1
+
+mksusecd --create /srv/www/admin/isos/${uuid}.iso \
+	--vendor "Dipl.Ing. Peter Varkoly" \
+	--application "$uuid installer" \
+	/usr/share/cephalix/templates/iso/ \
+	/usr/share/cephalix/templates/CRANIX.iso
 
