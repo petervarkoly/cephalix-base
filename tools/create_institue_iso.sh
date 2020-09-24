b23a4765bb3958da7e374a6e2d63b54ebd365994
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2020-09-24 21:30:36 +0200

    Fix creating insitute iso. Shutdown after installing all packages if installing via usb

--- tools/create_institue_iso.sh
+++ tools/create_institue_iso.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 uuid=$1
 cp /srv/www/admin/configs/${uuid}.xml /usr/share/cephalix/templates/iso/cranix.xml
+sed 's/halt config:type="boolean">false/halt config:type="boolean">true/' /srv/www/admin/configs/${uuid}.xml > /usr/share/cephalix/templates/iso/cranix-usb.xml
 
 mksusecd --create /srv/www/admin/isos/${uuid}.iso \
 	--vendor "Dipl.Ing. Peter Varkoly" \
 	--application "$uuid installer" \
-	/usr/share/cephalix/templates/iso/ \
-	/usr/share/cephalix/templates/CRANIX.iso
+	/usr/share/cephalix/templates/CRANIX.iso \
+	/usr/share/cephalix/templates/iso/
 
