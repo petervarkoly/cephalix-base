e67e8c4e5ff61ac7358f876d53a93f2353bae21b
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2020-11-07 11:58:33 +0100

    Remove deprecated scripts. Add new script to collet the status of the institutes.

---
+++ sbin/cephalix_get_status_of_institutes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+for id in $( echo "SELECT id from CephalixInstitutes where not deleted='Y' " | /usr/bin/mysql  CRX )
+do
+        IP=$( /usr/sbin/crx_api_text.sh GET institutes/$id/ipVPN )
+        if [ "$IP" ]; then
+                ping -c 1 $IP
+                if [ $? = 0 ]; then
+                        ssh $IP  /usr/sbin/crx_system_status.sh > /tmp/institute-status-$id.json
+                        /usr/sbin/crx_api_post_file.sh institutes/$id/status /tmp/institute-status-$id.json
+                fi
+        fi
+done
+
